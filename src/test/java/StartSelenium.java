158788cedbba9dba7128d860539a0d45805e6359
MikeTestin9 <Michael_benin@hotmail.com>
2023-06-15 14:56:27 +0300

    04

--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -16,11 +17,29 @@ public class StartSelenium {
 
     @Test
     public void test(){
+                // old strategies vs new
+        wd.findElement(By.tagName("a"));
+        wd.findElement(By.cssSelector("a"));
+
+        wd.findElement(By.id("root"));
+        wd.findElement(By.cssSelector("#root")); // # == id
+
+        wd.findElement(By.className("container"));
+        wd.findElement(By.cssSelector(".container")); // . == class
+        //
+        wd.findElement(By.linkText("HOME"));
+        wd.findElement(By.partialLinkText("OM")); //part of text
+        wd.findElement(By.cssSelector("[href='/home']")); //css
+        wd.findElement(By.cssSelector("[href*='om']")); // * atribute contains some txt
+        wd.findElement(By.cssSelector("[href^='/ho']")); // ^ atr begins in that letters
+        wd.findElement(By.cssSelector("[href$='ome']")); //$ ends with some text
+        //
 
     }
 
     @AfterTest
     public void postcondition(){
+        wd.quit();
 
     }
 }
