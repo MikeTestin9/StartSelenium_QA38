f71f50e3a1a8151bc0793a7df1f77eea05695a0a
MikeTestin9 <Michael_benin@hotmail.com>
2023-06-19 10:12:25 +0300

    hw6

--- src/test/java/TestLeumiBank.java
+++ src/test/java/TestLeumiBank.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -20,12 +21,9 @@ public class TestLeumiBank {
 
     @Test
     public void testLeumi(){
-      //  System.out.println(wd.findElement(By.xpath("//span[.='פרטי']")).getText());
-      //  System.out.println(wd.findElement(By.xpath("//span[.=\u05E4\u05E8\u05D8\u05D9]")).getText());
-        System.out.println(wd.findElement(By.linkText("פרטי")).getText());
-
-
-
+            System.out.println(wd.findElement(By.xpath("//span[.='פרטי']")).getText());
+//            System.out.println(wd.findElement(By.xpath("//span[.=\u05E4\u05E8\u05D8\u05D9]")).getText());
+//            System.out.println(wd.findElement(By.linkText("פרטי")).getText());
 
     }
 
